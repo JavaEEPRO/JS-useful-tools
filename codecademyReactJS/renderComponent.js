30d2edae12dc97ff04d6b68e4c98927e7012a236
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-09-27 09:03:22 +0300

    Render A Component
    And fell down on his face at his feet, giving him thanks: and he was a Samaritan. (Luke 17:16)
    ReactDOM.render() will tell <MyComponentClass /> to call its render method.
    
    <MyComponentClass /> will call its render method, which will return the JSX element <h1>Hello world</h1>. ReactDOM.render() will then take that resulting JSX element, and add it to the virtual DOM. This will make "Hello world" appear on the screen.

---
+++ codecademyReactJS/renderComponent.js
@@ -0,0 +1,15 @@
+//And fell down on his face at his feet, giving him thanks: and he was a Samaritan. (Luke 17:16)
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class MyComponentClass extends React.Component {
+  render() {
+    return <h1>Hello world</h1>;
+  }
+}
+
+ReactDOM.render(
+	<MyComponentClass />, 
+	document.getElementById('app')
+);
