ab608b83367fad6a6c8f76efa52ff8dcdb155a20
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-09-27 08:12:22 +0300

    ES6 syntax for class and subclass
    And when he saw them, he said unto them, Go shew yourselves unto the priests. And it came to pass, that, as they went, they were cleansed.(Luke 17:14)

---
+++ codecademyReactJS/classAndSubclassES6.js
@@ -0,0 +1,22 @@
+//And when he saw them, he said unto them, Go shew yourselves unto the priests. 
+//And it came to pass, that, as they went, they were cleansed.(Luke 17:14)
+
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    toString() {
+        return `(${this.x}, ${this.y})`;
+    }
+}
+
+class ColorPoint extends Point {
+    constructor(x, y, color) {
+        super(x, y);
+        this.color = color;
+    }
+    toString() {
+        return super.toString() + ' in ' + this.color;
+    }
+}
