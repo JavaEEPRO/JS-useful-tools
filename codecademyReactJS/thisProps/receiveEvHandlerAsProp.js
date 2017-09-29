049e2b538e0628c7fddfd1b4091baf6155b8625f
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-09-29 21:53:55 +0300

    Receive an Event Handler as a prop
    Whoever seeks to save his life loses it, but whoever loses his life preserves it.(Luke 17:33)
    Receive an Event Handler as a prop
    Great! You just passed a function from <Talker /> to <Button />.
    
    In the code editor, select Button.js. Notice that Button renders a <button></button> element.
    
    If a user clicks on this <button></button> element, then you want your passed-in talk function to get called.
    
    That means that you need to attach talk to the <button></button> as an event handler.
    
    How do you do that? The same way that you attach any event handler to a JSX element: you give that JSX element a special attribute. The attribute's name should be something like onClick or onHover. The attribute's value should be the event handler that you want to attach.

---
+++ codecademyReactJS/thisProps/receiveEvHandlerAsProp.js
@@ -0,0 +1,41 @@
+//Whoever seeks to save his life loses it, but whoever loses his life preserves it.(Luke 17:33)
+
+-------------------------------------Button.js--------------------------------------------------------------
+import React from 'react';
+
+export class Button extends React.Component {
+  render() {
+    return (
+      <button onClick={this.props.talk}>
+        Click me!
+      </button>
+    );
+  }
+}
+------------------------------------------------------------------------------------------------------------
+
+
+--------------------------------------Talker.js-------------------------------------------------------------
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Button } from './Button';
+
+class Talker extends React.Component {
+  talk() {
+    let speech = '';
+    for (let i = 0; i < 10000; i++) {
+      speech += 'blah ';
+    }
+    alert(speech);
+  }
+  
+  render() {
+    return <Button talk={this.talk}/>;
+  }
+}
+
+ReactDOM.render(
+  <Talker />,
+  document.getElementById('app')
+);
+------------------------------------------------------------------------------------------------------------
