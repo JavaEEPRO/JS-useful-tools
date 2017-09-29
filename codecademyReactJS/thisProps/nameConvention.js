3368088768c8976c68f6acc5a6c77cb908c2e261
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-09-29 22:55:26 +0300

    Create nameConvention.js
    I tell you, in that night there will be two people in one bed. The one will be taken, and the other will be left.(Luke 17:34)

---
+++ codecademyReactJS/thisProps/nameConvention.js
@@ -0,0 +1,80 @@
+//I tell you, in that night there will be two people in one bed. The one will be taken, and the other will be left.(Luke 17:34)
+----------------------------------Talker.js--------------------------------------------------------------------------------------
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Button } from './Button';
+
+class Talker extends React.Component {
+  talk() {
+    let speech = '';
+    for (let i = 0; i < 10000; i++) {
+      speech += 'blah ';
+    }
+    alert(speech);
+  }
+  
+  render() {
+    return <Button talk={this.talk}/>;
+  }
+}
+
+ReactDOM.render(
+  <Talker />,
+  document.getElementById('app')
+);
+---------------------------------------------------------------------------------------------------------------------------------
+
+-------------------------------------------Button.js-----------------------------------------------------------------------------
+import React from 'react';
+
+export class Button extends React.Component {
+  render() {
+    return (
+      <button onClick={this.props.talk}>
+        Click me!
+      </button>
+    );
+  }
+}
+---------------------------------------------------------------------------------------------------------------------------------
+THIS.PROPS
+handleEvent, onEvent, and this.props.onEvent
+Let's talk about naming things.
+
+When you pass an event handler as a prop, as you just did, there are two names that you have to choose.
+
+Both naming choices occur in the parent component class - that is, in the component class that defines the event handler and passes it.
+
+The first name that you have to choose is the name of the event handler itself.
+
+Look at Talker.js, lines 6 through 12. This is our event handler. We chose to name it talk.
+
+The second name that you have to choose is the name of the prop that you will use to pass the event handler. This is the same thing as your attribute name.
+
+For our prop name, we also chose talk, as shown on line 15:
+
+return <Button talk={this.talk} />;
+These two names can be whatever you want. However, there is a naming convention that they often follow. You don't have to follow this convention, but you should understand it when you see it.
+
+Here's how the naming convention works: first, think about what type of event you are listening for. In our example, the event type was "click."
+
+If you are listening for a "click" event, then you name your event handler handleClick. If you are listening for a "keyPress" event, then you name your event handler handleKeyPress:
+
+class MyClass extends React.Component {
+  handleHover() {
+    alert('I am an event handler.');
+    alert('I will be called in response to "hover" events.');
+  }
+}
+Your prop name should be the word on, plus your event type. If you are listening for a "click" event, then you name your prop onClick. If you are listening for a "keyPress" event, then you name your prop onKeyPress:
+
+class MyClass extends React.Component {
+  handleHover() {
+    alert('I am an event handler.');
+    alert('I will listen for a "hover" event.');
+  }
+
+  render() {
+    return <Child onHover={this.handleHover} />;
+  }
+}
