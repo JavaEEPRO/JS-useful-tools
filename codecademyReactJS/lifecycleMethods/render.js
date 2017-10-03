7a34cc8cc37d09e48e03d2c7803199a76db585d1
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-04 00:34:54 +0300

    render
    They understood none of these things. This saying was hidden from them, and they didn't understand the things that were said.(Luke 18:34)
    
    MOUNTING LIFECYCLE METHODS
    render
    render is a lifecycle method!
    
    We won't go over render here - we've already talked about it plenty. However, you should understand how render fits into the mounting period. Whenever a component mounts, componentWillMount is called first, followed by render, followed by componentDidMount.
    
    render belongs to two categories: mounting lifecycle methods, and updating lifecycle methods. We'll cover updating lifecycle methods in the next lesson. But first, there's one final mounting lifecycle method!

---
+++ codecademyReactJS/lifecycleMethods/render.js
@@ -0,0 +1,11 @@
+
+
+//They understood none of these things. This saying was hidden from them, and they didn't understand the things that were said.(Luke 18:34)
+
+MOUNTING LIFECYCLE METHODS
+render
+render is a lifecycle method!
+
+We won't go over render here - we've already talked about it plenty. However, you should understand how render fits into the mounting period. Whenever a component mounts, componentWillMount is called first, followed by render, followed by componentDidMount.
+
+render belongs to two categories: mounting lifecycle methods, and updating lifecycle methods. We'll cover updating lifecycle methods in the next lesson. But first, there's one final mounting lifecycle method!
